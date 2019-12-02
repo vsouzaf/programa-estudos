4adfb33639cb538aa589a8ddc1df3d4f60531281
vsouzaf <valtemir.souza168@gmail.com>
2019-12-01 22:59:29 -0300

    [FEAT] Serviços do Programa de Estudos [FEAT] Criação do front end

--- vuejs/assets/js.js
+++ vuejs/assets/js.js
@@ -13,29 +13,36 @@ window.onload = () => {
             }
         },
         mounted() {
-            console.log("afff");
+            axios
+                .get('http://symfony.localhost/api/orgao/')
+                .then(response => (this.setDadosComboDoResponse(response.data, "orgaos")));
+
             axios
                 .get('http://symfony.localhost/api/banca/')
-                .then(response => (this.carregarbancas(response.data)))
+                .then(response => (this.setDadosComboDoResponse(response.data, "bancas")));
         },
         methods: {
-            carregarbancas(data) {
-                console.log(data);
+            setDadosComboDoResponse(data, nomePropriedade) {
+                this[nomePropriedade] = this.setDadosParaCombo(data, "id", "nome");
+            },
+            setDadosParaCombo(dados, campoValue, campoTexto) {
+                let listaCombo = [];
+
+                dados.forEach((item) => {
+                    listaCombo.push(
+                        {
+                            value: item[campoValue],
+                            text: item[campoTexto]
+                        }
+                    );
+                });
+
+                return listaCombo;
             },
             onSubmit(evt) {
                 evt.preventDefault()
                 alert(JSON.stringify(this.form))
-            },
-            onReset(evt) {
-                evt.preventDefault()
-                // Reset our form values
-                this.form.food = null
-                // Trick to reset/clear native browser form validation state
-                this.show = false;
-                this.$nextTick(() => {
-                    this.show = true;
-                })
             }
         }
     })
-}
+};
